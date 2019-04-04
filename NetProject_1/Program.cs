8e2c95edc17f53e284237dab6c62290814355a8f
HP <HP@USER>
2019-04-04 18:14:00 +0600

    Добавьте файлы проекта.

---
+++ NetProject_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetProject_Client
+{
+    class Program
+    {
+        static int port = 8080; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipPoint);
+
+                Console.Write("Введите сообщение:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("ответ сервера: " + builder.ToString());
+
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ NetProject_Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetProject_Server
+{
+    class Program
+    {
+        static int port = 8080;
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(endPoint);
+                socket.Listen(100);    
+               
+                while (true)
+                {
+                    Console.WriteLine("Сервер запущен. Ожидание подключений...");                    
+                    Socket acceptSocket = socket.Accept();
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    byte[] bytes;
+                    int byteRec;
+
+                    do
+                    {
+                        bytes = new byte[1024];
+                        byteRec = acceptSocket.Receive(bytes);
+                        stringBuilder.Append(Encoding.UTF8.GetString(bytes));
+                    } while (acceptSocket.Available > 0);
+
+                    Console.WriteLine("Message: {0}", stringBuilder.ToString());
+
+                    string answer = string.Format("ваше сообщение {0} доставлено",
+                        stringBuilder.Length);
+
+                    byte[] reciveMsg = Encoding.UTF8.GetBytes(answer);
+
+                    acceptSocket.Send(reciveMsg);
+                    acceptSocket.Shutdown(SocketShutdown.Both);
+                    acceptSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
